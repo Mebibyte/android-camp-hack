1eb7b07e2399cd25e22201ca6957ee5a987cc9fe
Aisha Ferrazares <aisha.ferrazares@gmail.com>
2013-06-13 21:18:31 -0700

    task fix

--- CodersBestFriend/src/com/HACK/codersbestfriend/TaskManager.java
+++ CodersBestFriend/src/com/HACK/codersbestfriend/TaskManager.java
@@ -3,6 +3,7 @@ package com.HACK.codersbestfriend;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 
 /**
@@ -13,7 +14,7 @@ public class TaskManager {
         ArrayList<LinkedHashMap<String, String>> ans = new ArrayList<LinkedHashMap<String, String>>();
         if (tasks == null) return ans;
         for (LinkedHashMap<String, String> item : tasks) {
-            Log.i("TASK", "" + "Title: " + item.get("Title") + "Tags: " + item.get("Tags"));
+            Log.i("TASK", "" + "Title: " + item.get("Title") + "Tags: " + item.get("Tags") + " Keys: " + Arrays.toString(item.keySet().toArray()));
             if (!item.get("Title").equals("") && !item.get("Tags").equals("")) {
                 Task task = new Task(item.get("Title"), item.get("Tags"));
                 if (task.hasTag(tag)) ans.add(item);
